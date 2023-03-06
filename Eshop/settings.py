5c4b5335457b7e7e4a49208873df70259d79e338
Donald <netshiku@gmail.com>
2023-03-06 16:32:21 +0200

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -76,10 +76,17 @@ WSGI_APPLICATION = 'Eshop.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'E_COMMERCE',  
+        'USER': 'root',  
+        'PASSWORD': '0000',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }  
 }
 
 
